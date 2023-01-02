f83eb3c7800f69bba7dcdc3c4c397f09c0124bfa
Ukn Subedi <yukensubedi@gmail.com>
2023-01-02 21:54:29 +0545

    Product displayed from server

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.home),
-]
+    path("", views.home,name="home"),
+    path("about/", views.about,name="about"),
+    path("contact/", views.contact,name="contact"),
+    path("category/<slug:val>", views.CategoryView.as_view(),name = "category"),
+    path("category-title/<val>", views.CategoryTitle.as_view(),name = "category-title"),
+    path("product-detail/<int:pk>", views.ProductDetail.as_view(),name = "product-detail"),
+
+    # login authentication
+    path("registration/", views.CustomerRegistrationView.as_view(),name = "customerregistration"),
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ec/app/views.py
+++ ec/app/views.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render 
-
+from django.views import View
+from . models import Product
+from django.db.models import Count
+from . forms import CustomerRegistrationForm
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request,"app/home.html")
+
+def about(request):
+    return render(request,"app/about.html")
+
+def contact(request):
+    return render(request,"app/contact.html")
+
+
+class CategoryView(View):
+    def get(self,request,val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request , "app/category.html",locals())
+
+class CategoryTitle(View):
+    def get(self,request,val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request , "app/category.html",locals())
+
+
+class ProductDetail(View):
+    def get(self,request,pk):
+        product = Product.objects.get(pk=pk)
+
+        return render(request , "app/productdetail.html",locals())
+
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html',locals())
+    def post(self,request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Congratulations! User Registered Successfully")
+        else: 
+            messages.warning(request,"Invalid Input Data")
+        return render(request, 'app/customerregistration.html',locals())
